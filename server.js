fdfd7a20caba03a2e045b40ef7945b0058bc328a
listofbanned <listofbanned@gmail.com>
2019-09-14 17:24:05 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const Express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = Express()
+
+app.use('/graphql', expressGraphQL({
+	schema,
+	graphiql: true
+}))
+
+app.listen(4000, () => {
+	console.log('Listening')
+})
